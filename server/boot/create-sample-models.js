6e1e170177cf9f2cbd318bbbad2f00ca0ecb9d5d
Zhe Zhang <neomana@gmail.com>
2016-07-01 00:39:22 +0800

    created new model, data source & sample data

---
+++ server/boot/create-sample-models.js
@@ -0,0 +1,14 @@
+module.exports = function (app) {
+  app.dataSources.devDB.automigrate('talk', function (err) {
+    if (err) throw err
+
+    app.models.talk.create([
+      {title: 'React the good part', description: 'Good part about React.js', username: 'Michael', rating: 0},
+      {title: 'React the bad part', description: 'Bad part about React.js', username: 'Michael', rating: 0}
+    ], function (err, talks) {
+      if (err) throw err
+
+      console.log('Models created: \n', talks)
+    })
+  })
+}
