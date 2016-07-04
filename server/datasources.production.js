6f84bb62c95f7bddf0b438ce579b8cce7e58845e
Zhe Zhang <neomana@gmail.com>
2016-07-04 17:32:55 +0800

    fixed datasource file

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,12 +1,12 @@
-{
-  "prodDB": {
-    "host": process.env.MYSQL_PORT_3306_TCP_ADDR,
-    "port": process.env.MYSQL_PORT_3306_TCP_PORT,
-    "url": "",
-    "database": process.env.MYSQL_INSTANCE_NAME,
-    "password": process.env.MYSQL_PASSWORD,
-    "name": "prodDB",
-    "user":  process.env.MYSQL_USERNAME,
-    "connector": "mysql"
+module.exports = {
+  prodDB: {
+    host: process.env.MYSQL_PORT_3306_TCP_ADDR,
+    port: process.env.MYSQL_PORT_3306_TCP_PORT,
+    url: '',
+    database: process.env.MYSQL_INSTANCE_NAME,
+    password: process.env.MYSQL_PASSWORD,
+    name: 'prodDB',
+    user: process.env.MYSQL_USERNAME,
+    connector: 'mysql'
   }
 }
